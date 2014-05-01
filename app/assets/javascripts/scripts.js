ef6832f9834d9745184cb7c885535f8ead415cef
Hunter Meyer <huntermeyer@live.com>
2014-05-01 15:16:19 -0400

    Algin search bar

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,5 +1,5 @@
 $('#courses').dataTable({
   "sPaginationType": "foundation",
-  "sDom": "<'row'<'filter'<'filter-content'<'large-3'f>r>>>t<'row-fluid'<'large-12'p>>"
+  "sDom": "<'row'<'filter'<'filter-content'<'large-12 medium-12 small-12 columns'f>r>>>t<'row-fluid'<'large-12'p>>"
 });
 $('#courses_filter').children().children().attr("placeholder", "course, school...");
