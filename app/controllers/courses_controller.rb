d0b7f8c4e32d4b02af308c4c8b67509ae1d53acb
Hunter Meyer <huntermeyer@live.com>
2014-04-30 20:53:40 -0400

    Create courses controller

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,49 @@
+class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.create(course_params)
+    if @course.save
+      flash[:success] = 'Course Created'
+      redirect_to course_path
+    else
+      flash[:warning] = 'Something is missing'
+      render 'new'
+    end
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    if @course.update(course_parmas)
+      flash[:success] = 'Course Updated'
+      redirect_to course_path
+    else
+      flash[:warning] = 'Something is missing.'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Course.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+  def course_params
+    params.require(:course).permit()
+  end
+end
