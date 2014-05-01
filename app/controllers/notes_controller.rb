d4bd600e53af903062f2c245e4a3b8480ffe991c
Hunter Meyer <huntermeyer@live.com>
2014-04-30 21:04:28 -0400

    Create notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,21 @@
 class NotesController < ApplicationController
+  
+  def create
+    @note = Note.create(note_params)
+    if @note.save
+      flash[:success] = 'Note Saved'
+      redirect_to course_path(params[:id] => @note.course_id)
+    else
+      render 'new'
+    end
+  end
+
   def destroy
     Note.find(params[:id]).destroy
+  end
+
+  private
+  def note_params
+    params.require(:note).permit(:course_id, :content)
   end
 end
