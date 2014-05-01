3ab5fdacd66c60caf18d6d6e2c821a082be91f40
Hunter Meyer <huntermeyer@live.com>
2014-04-30 21:56:22 -0400

    Add placeholder to search bar

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,3 +1,5 @@
 $('#courses').dataTable({
-  "sPaginationType": "foundation"
+  "sPaginationType": "foundation",
+  "sDom": "<'row'<'filter'<'filter-content'<'large-3'f>r>>>t<'row-fluid'<'large-12'p>>"
 });
+$('#courses_filter').children().children().attr("placeholder", "course, school...");
