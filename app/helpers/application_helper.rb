f8f57368078db4fa1bd99b0c562ef903769c720e
Hunter Meyer <huntermeyer@live.com>
2014-05-06 19:20:08 -0400

    Add markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = 'MyWebEd'
+    if page_title.empty?
+      "#{base_title} | Tracking your Progress Through Online Education Courses" 
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
+  def markdown(text)
+    html = Redcarpet::Render::HTML.new(:prettify => true)
+    markdown = Redcarpet::Markdown.new(html, :space_after_headers => true,
+                                             :fenced_code_blocks => true)
+    markdown.render(text).html_safe
+  end
+
 end
