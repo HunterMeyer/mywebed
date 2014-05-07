5e1ea3430d9d685edf2f320bf444fa0d851713d7
Hunter Meyer <huntermeyer@live.com>
2014-05-07 10:21:59 -0400

    Sort courses table with started courses at the top

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,5 +1,6 @@
 $('#courses').dataTable({
   "sPaginationType": "foundation",
-  "sDom": "<'row'<'filter'<'filter-content'<'large-12 medium-12 small-12 columns'f>r>>>t<'row-fluid'<'large-12'p>>"
+  "sDom": "<'row'<'filter'<'filter-content'<'large-12 medium-12 small-12 columns'f>r>>>t<'row-fluid'<'large-12'p>>",
+  "aaSorting": [[2, "desc"]]
 });
 $('#courses_filter').children().children().attr("placeholder", "course, school...");
