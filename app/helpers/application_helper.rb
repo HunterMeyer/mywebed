61977ff328a3e6361790755e0abf344b8e521fa7
Hunter Meyer <huntermeyer@live.com>
2014-05-06 20:28:33 -0400

    Force carriage return on enter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,9 +10,9 @@ module ApplicationHelper
   end
 
   def markdown(text)
-    html = Redcarpet::Render::HTML.new(:prettify => true)
-    markdown = Redcarpet::Markdown.new(html, :space_after_headers => true,
-                                             :fenced_code_blocks => true)
+    html = Redcarpet::Render::HTML.new(prettify: true, hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(html, space_after_headers: true,
+                                             fenced_code_blocks: true)
     markdown.render(text).html_safe
   end
 
