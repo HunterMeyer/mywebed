84d18f1cd7e2df4bf648f19cebd9e92a06a3139e
Hunter Meyer <huntermeyer@live.com>
2014-10-03 13:33:57 -0400

    Config markdown for notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,8 @@
 class Note < ActiveRecord::Base
   belongs_to :course
+
+  def markdown
+    html = Redcarpet::Render::HTML.new(prettify: true, hard_wrap: true)
+    Redcarpet::Markdown.new(html, space_after_headers: true, fenced_code_blocks: true).render(self.content)
+  end
 end
