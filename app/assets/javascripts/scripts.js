49a4fa5abe4d350167b8ec67672f32ecc7b8ccb8
Hunter Meyer <huntermeyer@live.com>
2014-10-12 13:12:36 -0400

    Update datatable for additional column

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,7 +1,7 @@
 $('#courses').dataTable({
   "sPaginationType": "foundation",
   "sDom": "<'row'<'filter'<'filter-content'<'large-12 medium-12 small-12 columns'f>r>>>t<'row-fluid'<'large-12'p>>",
-  "aaSorting": [[2, "desc"]]
+  "aaSorting": [[3, "desc"]]
 });
 $('#courses_filter').children().children().attr("placeholder", "course, school...");
 
